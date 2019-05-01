276318ea8da4d9993045a37e52e65e0a8219d874
Gabi Hyman <19hymangabrielle@bprep.org>
2019-05-01 10:07:35 -0700

    Added tosSTing

---
+++ src/Tester.java
@@ -0,0 +1,8 @@
+public class Tester {
+    public static void main(String[] args) {
+        Card x = new Card("10", 10);
+        System.out.println(x);
+        Card y = new Card("K", 10);
+        System.out.println(y);
+    }
+}
