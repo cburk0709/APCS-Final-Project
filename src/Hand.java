b90348503ad7d4c8abe7265b7d4fd81602428635
ubuntustudent <janielsupreme@gmail.com>
2019-05-21 08:48:10 -0700

    very nice

--- src/Hand.java
+++ src/Hand.java
@@ -30,6 +30,10 @@ public class Hand implements Controls{
         cards.add(b);
     }
 
+    public void printFirstCard(){
+        System.out.println(cards.get(0).toString());
+    }
+
 
     //prints each Card in Hand object
     public void printHand(){
