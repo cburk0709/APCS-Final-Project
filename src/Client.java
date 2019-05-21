ed6cf14e983abd723c8c3481577b10c0efc78022
fruit-zip <cburk0709@gmail.com>
2019-05-21 08:16:59 -0700

    added construction of the deck to client

--- src/Client.java
+++ src/Client.java
@@ -9,9 +9,13 @@ public class Client {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         String conditional = "";
+        int[] myValues = {1, 2, 3, 4, 5, 6, 7 , 8, 9, 10, 11};
+        String[] myRanks = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"};
+        Deck myDeck = new Deck(myRanks, myValues);
         while (conditional != "quit"){
-            System.out.println();
-            //put some stuff
+            System.out.println("Welcome to Blackjack. To play, enter any token besides \"Q\"");
+            conditional = userInput.next();
+
        //     while(totalCount <= 21){
 
          //   }
