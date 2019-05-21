e262d4a1146e7197b8cdcb5fb5c6dc96a165f510
ubuntustudent <janielsupreme@gmail.com>
2019-05-21 08:33:27 -0700

    hit boi

--- src/Hand.java
+++ src/Hand.java
@@ -1,21 +1,37 @@
 import java.util.*;
 public class Hand implements Controls{
-    //ace will change to 1 from 11 if you bust
+
+    //aces will change from 1 to 11 if you bust; each ace can decrease totalValue by 10 if you bust
     int aces=0;
+
+    //totalValue is the highest value the hand can have that's less than or equal to 21
     int totalValue;
+
+    //cards stores all of the Card objects that belong in a Hand object
     ArrayList<Card> cards = new ArrayList<>();
+
+    //the constructor for Hand takes two Cards, anticipating use of <deck name>.deal() within constructor
     public Hand(Card a, Card b){
+        //records if either Card is an ace
         if (a.pointValue()==11){
             aces++;
         }
         if (b.pointValue()==11){
             aces++;
         }
+        //updates totalValue; if you have 22 assumes you have an ace and updates
         totalValue=a.pointValue()+b.pointValue();
+        if(totalValue==22){
+            aces--;
+            totalValue-=10;
+        }
+        //adds both Cards to cards
         cards.add(a);
         cards.add(b);
     }
 
+
+    //prints each Card in Hand object
     public void printHand(){
         for (Card card : cards){
             System.out.println(card.toString());
@@ -23,8 +39,12 @@ public class Hand implements Controls{
         System.out.println("Total Value: "+totalValue);
     }
 
+
+    //hit uses a Deck parameter to deal from that Deck
+
+    //might cause problems with not changing the actual Deck class in client code
     public void hit(Deck d){
-        //This might cause a problem if deck is empty; Card might not be able to be set to null
+        //might cause a problem if deck is empty; Card might not be able to be set to null
         Card c = d.deal();
         if (c==null){
             System.out.println("Deck is empty, game is over.");
@@ -35,6 +55,7 @@ public class Hand implements Controls{
             cards.add(c);
             printHand();
         }
+        //updates totalValue; checks for bust and if the player busts changes aces, if any, from 11 to 1
         totalValue+=c.pointValue();
         while(totalValue>21){
             if (aces>0){
@@ -46,6 +67,7 @@ public class Hand implements Controls{
         }
         System.out.println("Highest hand value is "+totalValue+".");
     }
+
     public void stay(){
         System.out.println("Player stays.");
     }
